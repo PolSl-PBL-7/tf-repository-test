cae606938276f5b77fd36d11bb7ed5dfa5897ca7
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-11-06 14:56:35 +0100

    feat: added tensorflow hack for macos

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,10 +1,10 @@
 import pytest
-
+import sys
+import os
+sys.path.append(os.getenv("PYTHONPATH"))
 
 def test_import_tensorflow() -> None:
-    try:
-        import tensorflow as tf
-        if not tf.test.gpu_device_name():
-            pytest.fail("No GPU detected")
-    except Exception as e:
-        pytest.fail(e)
+    import tensorflow as tf
+
+    if not tf.test.gpu_device_name():
+        pytest.fail("No GPU detected")
